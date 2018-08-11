ae6477d988e039d4ef674abe43624ae84e3536f2
Shreedeep Sawaikar <shreedeepsawaikar@gmail.com>
2018-08-11 12:00:48 +0200

    reducing difficulty

--- create-blockchain/blockchain.py
+++ create-blockchain/blockchain.py
@@ -1,6 +1,6 @@
-#module 1 create a blockchain
+# module 1 create a blockchain
 
-#Importing the libraries
+# Importing the libraries
 import datetime
 import hashlib
 import json
@@ -8,17 +8,18 @@ from flask import Flask, jsonify
 
 # Part 1 Building a Blockchain
 
+
 class Blockchain:
-    
+
     def __init__(self):
-        self.chain =  []
-        self.create_block(proof = 1, previous_hash = '0')
+        self.chain = []
+        self.create_block(proof=1, previous_hash='0')
         
     def create_block(self, proof, previous_hash):
-        block = {'index' : len(self.chain) + 1,
-                 'timestamp' : str(datetime.datetime.now()),
-                 'proof' : proof,
-                 'previous_hash' : previous_hash}
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
         
         self.chain.append(block)
         return block
@@ -32,19 +33,19 @@ class Blockchain:
         check_proof = False
         while check_proof is False:
             hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_operation[:4] == '000000':
+            if hash_operation[:4] == '0000':
                 check_proof = True
             else:
-               new_proof +=1
+                new_proof += 1
                
         return new_proof
-    
+
     def hash(self, block):
-        encoded_block = json.dumps(block, sort_keys = True).encode()
+        encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
-                
-         
-    def is_chain_valid(self, chain):
+
+
+def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
         while block_index < len(chain):
@@ -54,10 +55,10 @@ class Blockchain:
             previous_proof = previous_block['proof']
             proof = block['proof']
             hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_operation[:4] != '000000':
+            if hash_operation[:4] != '0000':
                 return False
             previous_block = block
-            block_index +=1
+            block_index += 1
         return True
             
 
@@ -72,6 +73,7 @@ app = Flask(__name__)
 blockchain = Blockchain()
 
 # Mining a new block
+
 
 @app.route('/mine_block', methods=['GET'])
 def mine_block():
@@ -110,7 +112,6 @@ def is_valid():
     return jsonify(response),200
 
 
-#Running the app
-app.run(host = '0.0.0.0', port=5000) 
-          
-      
+# Running the app
+
+app.run(host='0.0.0.0', port=5000)
